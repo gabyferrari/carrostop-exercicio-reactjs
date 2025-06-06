d4fa23e687ab767f1394c8dc1c188da0f3784061
Gabrielly Ferrari <gaby.ferrari@outlook.com.br>
2025-06-06 16:19:51 -0300

    Header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import './styles.css'
+
 export default function Header() {
 
     return (
         <header>
-            <div>
+            <div className="ct-container">
                 <h1>Carros Top</h1>
             </div>   
         </header>
